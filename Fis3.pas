d52da64fc6f8f429ca2460f16053a7b4b6a8481f
sayazid <120023680+sayazid@users.noreply.github.com>
2022-12-07 19:19:23 +0700

    Create Fis3.pas

---
+++ Fis3.pas
@@ -0,0 +1,15 @@
+program no_3;
+    
+var
+    v: integer;
+    i, r: real;
+
+begin
+    writeln('NO. 3');
+    writeln('-Hambatan Toaster-');
+    writeln('Diketahui V = 110, I = 2,8');
+    v := 110;
+    i := 2.8;
+    r := v / i;
+    writeln('Hasil nya = ', r:1:3, ' Ohm');
+end.
